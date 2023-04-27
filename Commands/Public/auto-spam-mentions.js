551ed38da3ef8beab5a502ff2397492151c26cd3
Deadlock <87986054+deadlockjs@users.noreply.github.com>
2023-04-27 03:05:27 +0300

    Update auto-spam-mentions.js

--- Commands/Public/auto-spam-mentions.js
+++ Commands/Public/auto-spam-mentions.js
@@ -3,7 +3,7 @@ const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
         data: new SlashCommandBuilder()
-        .setName('automode-spam-mentions')
+        .setName('automod-spam-mentions')
         .setDescription('Creates an automated rule for auto mentions')
         .addIntegerOption(option => option.setName('spam-mention').setDescription('Add the number to remove messages from suspected spammers!').setRequired(true)),
 
@@ -16,7 +16,7 @@ async execute (interaction , client) {
         const rule = await guild.autoModerationRules.create(
             {
                 name: `Prevent spam mentions by ${client.user.username}`,
-                creatorId: `youraccountid`,
+                creatorId: `487229623810129922`,
                 enabled: true,
                 eventType: 1,
                 triggerType: 5,
